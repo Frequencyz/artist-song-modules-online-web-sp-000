8f6b5d06afbadf5e738e87949c349c0fe8ff677c
Andrew Bay <daddnbay@yahoo.com>
2019-07-30 20:54:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,9 +29,9 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end
 
 class Artist
