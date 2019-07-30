bcdd21db3e384b296e0f55273598b9c30200b13f
Andrew Bay <daddnbay@yahoo.com>
2019-07-30 21:29:58 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,17 @@
 module Memorable
+  module ClassMethods
+   def reset_all
+     self.all.clear
+   end
   
-  def reset_all
-    all.clear
+   def count
+     self.all.count
+   end
+end
+
+module InstanceMethods
+  def initialize
+    
   end
-  
-  def count
-    all.count
-  end
+ end
 end
