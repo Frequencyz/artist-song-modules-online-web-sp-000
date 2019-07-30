29982a3f1145e23e4ad3d85d30c6e2a72520e73d
Andrew Bay <daddnbay@yahoo.com>
2019-07-30 20:32:41 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,10 +3,10 @@ require_relative '../lib/concerns/memorable'
 module Memorable
   
   def self.reset_all
-    
+    self.all.clear
   end
   
   def self.count
-    
+    self.all.count
   end
 end
